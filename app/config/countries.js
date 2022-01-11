831b87cbc595342962798039e60df81dfe405e48
vnshgrg <ghale.avinash@gmail.com>
2022-01-11 16:54:55 +0900

    added basic config and flags for new countries

--- app/config/countries.js
+++ app/config/countries.js
@@ -1,12 +1,18 @@
 export const countries = {
+    ARM: "Armenia",
     AUS: "Australia",
+    AZE: "Azerbaijan",
     BGD: "Bangladesh",
+    BLR: "Belarus",
     IDN: "Indonesia",
     IND: "India",
-    JPN: "Japan",
+    KAZ: "Kazakhstan",
+    KGZ: "Kyrgyzstan",
     KOR: "South Korea",
     LKA: "Sri Lanka",
+    MDA: "Moldova",
     MMR: "Myanmar",
+    MNG: "Mongolia",
     MYS: "Malaysia",
     NPL: "Nepal",
     PAK: "Pakistan",
@@ -14,17 +20,27 @@ export const countries = {
     RUS: "Russia",
     SGP: "Singapore",
     THA: "Thailand",
+    TJK: "Tajikistan",
+    UKR: "Ukraine",
+    UZB: "Uzbekistan",
     VNM: "Viet Nam",
 };
 
 export const countriesFlag = {
+    ARM: renderFlag("ARM"),
     AUS: renderFlag("AUS"),
+    AZE: renderFlag("AZE"),
     BGD: renderFlag("BGD"),
+    BLR: renderFlag("BLR"),
     IDN: renderFlag("IDN"),
     IND: renderFlag("IND"),
+    KAZ: renderFlag("KAZ"),
+    KGZ: renderFlag("KGZ"),
     KOR: renderFlag("KOR"),
     LKA: renderFlag("LKA"),
+    MDA: renderFlag("MDA"),
     MMR: renderFlag("MMR"),
+    MNG: renderFlag("MNG"),
     MYS: renderFlag("MYS"),
     NPL: renderFlag("NPL"),
     PAK: renderFlag("PAK"),
@@ -32,11 +48,19 @@ export const countriesFlag = {
     RUS: renderFlag("RUS"),
     SGP: renderFlag("SGP"),
     THA: renderFlag("THA"),
+    TJK: renderFlag("TJK"),
+    UKR: renderFlag("UKR"),
+    UZB: renderFlag("UZB"),
     VNM: renderFlag("VNM"),
 };
 
 export function renderFlag(CODE) {
     return <SvgFlag code={CODE} />;
+
+    // if (supportsFlagEmoji()) {
+    //     return emojiFlags[CODE];
+    // } else {
+    // }
 }
 
 export function SvgFlag({ code }) {
@@ -49,13 +73,48 @@ export function SvgFlag({ code }) {
 }
 
 export const serviceCharge = {
+    ARM: {
+        type: { bank: true, cash: false },
+        bank: [
+            {
+                min: 5000,
+                max: 301500,
+                serviceCharge: 1500,
+            },
+            {
+                min: 301001,
+                max: 1002000,
+                serviceCharge: 2000,
+            },
+        ],
+    },
     AUS: {
         type: { bank: true, cash: false },
         bank: [
             {
                 min: 5000,
-                max: 5500000,
-                serviceCharge: 5000,
+                max: 301500,
+                serviceCharge: 1500,
+            },
+            {
+                min: 301001,
+                max: 1002000,
+                serviceCharge: 2000,
+            },
+        ],
+    },
+    AZE: {
+        type: { bank: true, cash: false },
+        bank: [
+            {
+                min: 5000,
+                max: 301500,
+                serviceCharge: 1500,
+            },
+            {
+                min: 301001,
+                max: 1002000,
+                serviceCharge: 2000,
             },
         ],
     },
@@ -64,15 +123,45 @@ export const serviceCharge = {
         cash: [
             {
                 min: 5000,
-                max: 5500000,
-                serviceCharge: 5000,
+                max: 50500,
+                serviceCharge: 500,
+            },
+            {
+                min: 50501,
+                max: 300000,
+                serviceCharge: 950,
             },
         ],
         bank: [
             {
                 min: 5000,
-                max: 5500000,
-                serviceCharge: 5000,
+                max: 50500,
+                serviceCharge: 500,
+            },
+            {
+                min: 50501,
+                max: 300950,
+                serviceCharge: 950,
+            },
+            {
+                min: 300951,
+                max: 1001500,
+                serviceCharge: 1500,
+            },
+        ],
+    },
+    BLR: {
+        type: { bank: true, cash: false },
+        bank: [
+            {
+                min: 5000,
+                max: 301500,
+                serviceCharge: 1500,
+            },
+            {
+                min: 301001,
+                max: 1002000,
+                serviceCharge: 2000,
             },
         ],
     },
@@ -81,8 +170,18 @@ export const serviceCharge = {
         bank: [
             {
                 min: 5000,
-                max: 5500000,
-                serviceCharge: 5000,
+                max: 30750,
+                serviceCharge: 750,
+            },
+            {
+                min: 30751,
+                max: 301450,
+                serviceCharge: 1450,
+            },
+            {
+                min: 301451,
+                max: 1001780,
+                serviceCharge: 1780,
             },
         ],
     },
@@ -91,8 +190,48 @@ export const serviceCharge = {
         bank: [
             {
                 min: 5000,
-                max: 5500000,
-                serviceCharge: 5000,
+                max: 30500,
+                serviceCharge: 500,
+            },
+            {
+                min: 30501,
+                max: 301000,
+                serviceCharge: 1000,
+            },
+            {
+                min: 301001,
+                max: 1001500,
+                serviceCharge: 1500,
+            },
+        ],
+    },
+    KAZ: {
+        type: { bank: true, cash: false },
+        bank: [
+            {
+                min: 5000,
+                max: 301500,
+                serviceCharge: 1500,
+            },
+            {
+                min: 301001,
+                max: 1002000,
+                serviceCharge: 2000,
+            },
+        ],
+    },
+    KGZ: {
+        type: { bank: true, cash: false },
+        bank: [
+            {
+                min: 5000,
+                max: 301500,
+                serviceCharge: 1500,
+            },
+            {
+                min: 301001,
+                max: 1002000,
+                serviceCharge: 2000,
             },
         ],
     },
@@ -100,9 +239,14 @@ export const serviceCharge = {
         type: { bank: true, cash: false },
         bank: [
             {
-                min: 5000,
-                max: 5500000,
-                serviceCharge: 5000,
+                min: 1000,
+                max: 100500,
+                serviceCharge: 500,
+            },
+            {
+                min: 100501,
+                max: 501000,
+                serviceCharge: 1000,
             },
         ],
     },
@@ -111,15 +255,35 @@ export const serviceCharge = {
         bank: [
             {
                 min: 5000,
-                max: 5500000,
-                serviceCharge: 5000,
+                max: 100800,
+                serviceCharge: 800,
+            },
+            {
+                min: 100801,
+                max: 601800,
+                serviceCharge: 1800,
             },
         ],
         cash: [
             {
                 min: 5000,
-                max: 5500000,
-                serviceCharge: 5000,
+                max: 130800,
+                serviceCharge: 800,
+            },
+        ],
+    },
+    MDA: {
+        type: { bank: true, cash: false },
+        bank: [
+            {
+                min: 5000,
+                max: 301500,
+                serviceCharge: 1500,
+            },
+            {
+                min: 301001,
+                max: 1002000,
+                serviceCharge: 2000,
             },
         ],
     },
@@ -128,8 +292,33 @@ export const serviceCharge = {
         bank: [
             {
                 min: 5000,
-                max: 5500000,
-                serviceCharge: 5000,
+                max: 100850,
+                serviceCharge: 850,
+            },
+            {
+                min: 100851,
+                max: 301200,
+                serviceCharge: 1200,
+            },
+            {
+                min: 301201,
+                max: 1001800,
+                serviceCharge: 1800,
+            },
+        ],
+    },
+    MNG: {
+        type: { bank: true, cash: false },
+        bank: [
+            {
+                min: 5000,
+                max: 301500,
+                serviceCharge: 1500,
+            },
+            {
+                min: 301001,
+                max: 1002000,
+                serviceCharge: 2000,
             },
         ],
     },
@@ -137,9 +326,19 @@ export const serviceCharge = {
         type: { bank: true, cash: false },
         bank: [
             {
-                min: 5000,
-                max: 5500000,
-                serviceCharge: 5000,
+                min: 2000,
+                max: 100700,
+                serviceCharge: 700,
+            },
+            {
+                min: 100701,
+                max: 501200,
+                serviceCharge: 1200,
+            },
+            {
+                min: 501201,
+                max: 1001800,
+                serviceCharge: 1800,
             },
         ],
     },
@@ -147,16 +346,36 @@ export const serviceCharge = {
         type: { bank: true, cash: true },
         cash: [
             {
-                min: 5000,
-                max: 5700000,
-                serviceCharge: 5000,
+                min: 2000,
+                max: 30500,
+                serviceCharge: 500,
+            },
+            {
+                min: 30501,
+                max: 301000,
+                serviceCharge: 1000,
+            },
+            {
+                min: 301001,
+                max: 1001500,
+                serviceCharge: 1500,
             },
         ],
         bank: [
             {
-                min: 5000,
-                max: 5700000,
-                serviceCharge: 5000,
+                min: 1500,
+                max: 30500,
+                serviceCharge: 500,
+            },
+            {
+                min: 30501,
+                max: 301000,
+                serviceCharge: 1000,
+            },
+            {
+                min: 301001,
+                max: 1001500,
+                serviceCharge: 1500,
             },
         ],
     },
@@ -165,15 +384,35 @@ export const serviceCharge = {
         cash: [
             {
                 min: 5000,
-                max: 5500000,
-                serviceCharge: 5000,
+                max: 30500,
+                serviceCharge: 500,
+            },
+            {
+                min: 30501,
+                max: 300800,
+                serviceCharge: 800,
+            },
+            {
+                min: 300801,
+                max: 381200,
+                serviceCharge: 1200,
             },
         ],
         bank: [
             {
-                min: 5000,
-                max: 5500000,
-                serviceCharge: 5000,
+                min: 3000,
+                max: 30500,
+                serviceCharge: 500,
+            },
+            {
+                min: 30501,
+                max: 300800,
+                serviceCharge: 800,
+            },
+            {
+                min: 300801,
+                max: 1001200,
+                serviceCharge: 1200,
             },
         ],
     },
@@ -181,16 +420,31 @@ export const serviceCharge = {
         type: { bank: true, cash: true },
         cash: [
             {
-                min: 5000,
-                max: 5500000,
-                serviceCharge: 5000,
+                min: 4000,
+                max: 30450,
+                serviceCharge: 450,
+            },
+            {
+                min: 30451,
+                max: 120000,
+                serviceCharge: 850,
             },
         ],
         bank: [
             {
                 min: 5000,
-                max: 5500000,
-                serviceCharge: 5000,
+                max: 30450,
+                serviceCharge: 450,
+            },
+            {
+                min: 30451,
+                max: 300850,
+                serviceCharge: 850,
+            },
+            {
+                min: 300851,
+                max: 1001250,
+                serviceCharge: 1250,
             },
         ],
     },
@@ -199,8 +453,18 @@ export const serviceCharge = {
         cash: [
             {
                 min: 5000,
-                max: 5500000,
-                serviceCharge: 5000,
+                max: 101000,
+                serviceCharge: 1000,
+            },
+            {
+                min: 101001,
+                max: 3001500,
+                serviceCharge: 1500,
+            },
+            {
+                min: 3001501,
+                max: 10002000,
+                serviceCharge: 2000,
             },
         ],
     },
@@ -209,8 +473,13 @@ export const serviceCharge = {
         bank: [
             {
                 min: 5000,
-                max: 5500000,
-                serviceCharge: 5000,
+                max: 301000,
+                serviceCharge: 1000,
+            },
+            {
+                min: 301001,
+                max: 1001800,
+                serviceCharge: 1800,
             },
         ],
     },
@@ -219,8 +488,63 @@ export const serviceCharge = {
         bank: [
             {
                 min: 5000,
-                max: 5500000,
-                serviceCharge: 5000,
+                max: 100800,
+                serviceCharge: 800,
+            },
+            {
+                min: 100801,
+                max: 301200,
+                serviceCharge: 1200,
+            },
+            {
+                min: 301201,
+                max: 1001500,
+                serviceCharge: 1500,
+            },
+        ],
+    },
+    RJK: {
+        type: { bank: true, cash: false },
+        bank: [
+            {
+                min: 5000,
+                max: 301500,
+                serviceCharge: 1500,
+            },
+            {
+                min: 301001,
+                max: 1002000,
+                serviceCharge: 2000,
+            },
+        ],
+    },
+    UKR: {
+        type: { bank: true, cash: false },
+        bank: [
+            {
+                min: 5000,
+                max: 301500,
+                serviceCharge: 1500,
+            },
+            {
+                min: 301001,
+                max: 1002000,
+                serviceCharge: 2000,
+            },
+        ],
+    },
+    UZB: {
+        type: { bank: true, cash: false },
+        bank: [
+            {
+                min: 5000,
+                max: 301500,
+                serviceCharge: 1500,
+            },
+            {
+                min: 301001,
+                max: 1002000,
+                serviceCharge: 2000,
             },
         ],
     },
@@ -228,16 +552,46 @@ export const serviceCharge = {
         type: { bank: true, cash: true },
         cash: [
             {
-                min: 5000,
-                max: 5500000,
-                serviceCharge: 5000,
+                min: 1000,
+                max: 30480,
+                serviceCharge: 480,
+            },
+            {
+                min: 30481,
+                max: 250880,
+                serviceCharge: 880,
+            },
+            {
+                min: 250881,
+                max: 601280,
+                serviceCharge: 1280,
+            },
+            {
+                min: 601281,
+                max: 1001980,
+                serviceCharge: 1680,
             },
         ],
         bank: [
             {
-                min: 5000,
-                max: 5500000,
-                serviceCharge: 5000,
+                min: 1000,
+                max: 30480,
+                serviceCharge: 480,
+            },
+            {
+                min: 30481,
+                max: 250880,
+                serviceCharge: 880,
+            },
+            {
+                min: 250881,
+                max: 601280,
+                serviceCharge: 1280,
+            },
+            {
+                min: 601281,
+                max: 1001980,
+                serviceCharge: 1680,
             },
         ],
     },
