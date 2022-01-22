c479a35f9fe63be684c2e5e4fe2c54367dd9caab
vnshgrg <ghale.avinash@gmail.com>
2022-01-22 18:45:00 +0900

    changed api url

--- app/config/index.js
+++ app/config/index.js
@@ -1,3 +1,3 @@
 export * from "./countries";
 export const siteName = "QSRemit";
-export const NOTICE_API_URL = "http://notice.qsremit.net/graphql";
+export const NOTICE_API_URL = "https://notice.qsremit.net/graphql";
