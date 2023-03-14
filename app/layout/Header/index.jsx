675d2161dbdfd7a89a3c9b874b28953621125bd3
vnshgrg <ghale.avinash@gmail.com>
2023-03-14 22:43:46 +0900

    fixed slider

--- app/layout/Header/index.jsx
+++ app/layout/Header/index.jsx
@@ -1,27 +1,63 @@
 import React, { useEffect, useState, useRef } from "react";
 import { Navigation } from "../../components";
 
+const slides = [
+    "/backgrounds/01.jpg",
+    "/backgrounds/02.jpg",
+    "/backgrounds/03.jpg",
+    "/backgrounds/04.jpg",
+];
+const sliderConfig = {
+    slides,
+    totalSlides: slides.length,
+    interval: 5000,
+};
+
 const Header = ({ children }) => {
     const [heroHeight, setHeroHeight] = useState(0);
+    const [heroWidth, setHeroWidth] = useState(0);
+    const [slideIndex, setSlideIndex] = useState(0);
 
     const ref = useRef(null);
+
+    let interval;
+
+    const updateInterval = () => {
+        interval =
+            !interval &&
+            setInterval(() => {
+                setSlideIndex((slideIndex) => {
+                    if (slideIndex + 1 < sliderConfig.totalSlides) {
+                        return slideIndex + 1;
+                    } else {
+                        return 0;
+                    }
+                });
+            }, sliderConfig.interval);
+    };
 
     useEffect(() => {
         const handleResize = () => {
             if (heroHeight !== ref.current.clientHeight) {
                 setHeroHeight(ref.current.clientHeight);
-                console.log(ref.current.clientHeight);
+            }
+            if (heroWidth !== ref.current.clientWidth) {
+                setHeroWidth(ref.current.clientWidth);
             }
         };
 
-        setTimeout(() => {
+        const timeout = setTimeout(() => {
             handleResize();
         }, 500);
 
         window.addEventListener("resize", handleResize);
 
+        updateInterval();
+
         return () => {
             window.removeEventListener("resize", handleResize);
+            clearInterval(interval);
+            clearTimeout(timeout);
         };
     }, []);
 
@@ -29,46 +65,39 @@ const Header = ({ children }) => {
         <header>
             <div
                 ref={ref}
-                className="relative bg-gradient-to-br from-blue-400 to-blue-800"
+                className="relative bg-gradient-to-br from-primary to-yellow-600"
             >
                 <div
                     className="absolute flex flex-grow w-full overflow-hidden"
                     style={{ height: heroHeight }}
                 >
                     <div
-                        id="carouselExampleSlidesOnly"
-                        className="carousel slide relative"
-                        data-bs-ride="carousel"
+                        className="relative overflow-hidden"
+                        style={{
+                            height: heroHeight,
+                            width: heroWidth,
+                        }}
                     >
-                        <div className="carousel-inner relative w-full overflow-hidden">
-                            <div className="carousel-item active relative float-left w-full">
+                        {sliderConfig.slides.map((url, index) => (
+                            <div
+                                className={`absolute w-full top-0 left-0 right-0 bottom-0 transition-opacity duration-1000 ${
+                                    slideIndex === index
+                                        ? "opacity-100"
+                                        : "opacity-0"
+                                }`}
+                                key={index}
+                                style={{
+                                    height: heroHeight,
+                                    width: heroWidth,
+                                }}
+                            >
                                 <img
-                                    src="/backgrounds/01.jpg"
-                                    className="block w-full "
-                                    alt="Wild Landscape"
+                                    src={url}
+                                    className="object-cover h-full w-full "
+                                    alt="QS Remit"
                                 />
                             </div>
-                            <div className="carousel-item relative float-left w-full">
-                                <img
-                                    src="/backgrounds/02.jpg"
-                                    className="block w-full "
-                                />
-                            </div>
-                            <div className="carousel-item relative float-left w-full">
-                                <img
-                                    src="/backgrounds/03.jpg"
-                                    className="block w-full "
-                                    alt="Exotic Fruits"
-                                />
-                            </div>
-                            <div className="carousel-item relative float-left w-full">
-                                <img
-                                    src="/backgrounds/04.jpg"
-                                    className="block w-full "
-                                    alt="Exotic Fruits"
-                                />
-                            </div>
-                        </div>
+                        ))}
                     </div>
                 </div>
                 <div className="relative py-6">
